dd4a791485f73d80dc25a77be85600cf428129cd
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 19:19:12 +0000

    BaseTest setup

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,6 +1,10 @@
 package base;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.Homepage;
 
 public class BaseTests {
@@ -8,5 +12,18 @@ public class BaseTests {
     private EventFiringWebDriver driver;
     protected Homepage homepage;
 
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        goHome();
 
+        homepage = new Homepage(driver);
+    }
+
+    @BeforeMethod
+    public void goHome(){driver.get("https://news.sky.com/uk");}
+
+    @AfterClass
+    public void tearDown(){driver.quit();}
 }
