611c80948d1fbd1427e6357d3bd1b65835fecbfe
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 19:02:58 +0000

    added link to AccessabilityPage and clickLink

--- src/main/java/pages/Homepage.java
+++ src/main/java/pages/Homepage.java
@@ -1,10 +1,21 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class homepage {
+public class Homepage {
 
     private WebDriver driver;
 
-    public homepage(WebDriver driver){this.driver = driver;}
+    public Homepage(WebDriver driver){this.driver = driver;}
+
+    public AccessabilityPage clickAccessabilityPage(){
+        clickLink("");
+        return new AccessabilityPage(driver);
+
+    }
+
+    public void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
 }
