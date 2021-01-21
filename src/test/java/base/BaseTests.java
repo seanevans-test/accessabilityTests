ff4cc488defc7769a25d5b78ebeac304d062311c
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 19:13:03 +0000

    first commit on BaseTest

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,12 @@
+package base;
+
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import pages.Homepage;
+
+public class BaseTests {
+
+    private EventFiringWebDriver driver;
+    protected Homepage homepage;
+
+
+}
