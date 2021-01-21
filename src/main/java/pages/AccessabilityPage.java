4e96ed4990127033921f71e2f7b398f548d5b476
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 19:02:22 +0000

    first commit on AccessabilityPage

---
+++ src/main/java/pages/AccessabilityPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AccessabilityPage {
+
+    private WebDriver driver;
+
+    public AccessabilityPage(WebDriver driver){this.driver = driver;}
+}
