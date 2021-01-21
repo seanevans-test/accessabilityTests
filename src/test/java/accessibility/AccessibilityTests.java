14ce90c0df303b2674daf365d04174bcfe96878b
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 19:26:38 +0000

    added link to AccessibilityPage on the test

--- src/test/java/accessibility/AccessibilityTests.java
+++ src/test/java/accessibility/AccessibilityTests.java
@@ -1,6 +1,13 @@
 package accessibility;
 
 import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.AccessibilityPage;
 
 public class AccessibilityTests extends BaseTests {
+
+    @Test
+    public void testAccessibilityButton(){
+        AccessibilityPage accessibilityPage = homepage.clickAccessibilityPage();
+    }
 }
